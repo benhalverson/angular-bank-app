6ca1b723bfbababcb3363cce66167a64ee117208
Ben Halverson <benhalverson33@gmail.com>
2015-04-22 01:10:23 -0700

    wip

--- client/index.js
+++ client/index.js
@@ -1,3 +1,25 @@
 'use strict';
 
-angular.module('poseidon', ['firebase']);
+angular.module('bank-app', ['firebase'])
+.controller('master', ['$scope', function($scope){
+  $scope.balance = 10000;
+  $scope.name = 'Ben';
+  $scope.deposits = [];
+  $scope.withdraws = [];
+  $scope.fees = [];
+
+  $scope.deposit = function(){
+
+    $scope.balance += $scope.amount;
+    $scope.deposits.push({amount:$scope.amount, date: new Date()});
+  };
+  $scope.withdraw = function(){
+    var fee = 0;
+    if($scope >= 0 && ($scope.balance - $scope.amount < 0)){
+      fee = 50;
+    }
+
+    $scope.balance -= $scope.amount + fee;
+    $scope.withdraws.push({amount:$scope.amount, date:new Date()});
+  };
+}]);
